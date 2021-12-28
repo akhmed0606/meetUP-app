8649de2a46dbd46c932ee405074da47987cf9b03
Akhmed <analgiev57@gmail.com>
2021-12-27 22:00:12 -0500

    Added facebook-share

--- src/components/meetups/MeetItem.js
+++ src/components/meetups/MeetItem.js
@@ -1,11 +1,20 @@
 import classes from "./MeetItem.module.css";
 import Card from "../ui/Card";
+import {FacebookShareButton}  from 'react-share';
 import { useContext } from "react";
 import { FcLike } from "react-icons/fc";
-import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai";
 import FavoriteContext from "../../store/favorite-context";
 
 function MeetupItem(props) {
+
+  const shareButtonProps = {
+    url: "https://www.facebook.com/",
+    network: "Facebook",
+
+  };
+
+
   const favoritesCtx = useContext(FavoriteContext);
 
   const itemIsFavorites = favoritesCtx.itemIsFavorites(props.id);
@@ -28,6 +37,16 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
+
+          <div className={classes.sharing}>
+            <button className={classes.sharing}>
+              <FacebookShareButton {...shareButtonProps}>
+              <AiOutlineShareAlt />
+              </FacebookShareButton>
+            
+            </button>
+          </div>
+
           <div className={classes.actions}>
             <button onClick={toggleFavorite} className={classes.actions}>
               {itemIsFavorites ? <FcLike /> : <AiOutlineHeart />}

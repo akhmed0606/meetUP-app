19e3993345c60814a5d63293a9fc88907a9aee1a
akhmed0606 <analgiev57gmail.com>
2022-01-25 12:23:56 -0500

    Tested all routes for correct path and render

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+
 import { MemoryRouter } from 'react-router';
 import App from './App'
 import AllMeetsPage from "./pages/AllMeet";
@@ -9,4 +10,55 @@ import Favorites from "./pages/Favorites";
 jest.mock('./pages/AllMeet');
 jest.mock('./pages/NewMeet');
 jest.mock('./pages/Favorites');
+           
+           //All Meetups
+test("Should render ALL meetups page on default route", () => {
 
+    // Arrange
+    AllMeetsPage.mockImplementation(() => <div>AllMeetupsPage</div>);
+
+    // Act
+    render(
+      <MemoryRouter initialEntries={[ '/' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+
+    // Assert
+    expect(screen.getByText("AllMeetupsPage")).toBeInTheDocument();
+    
+})
+              //New Meetups
+test("Should render NEW meet page on new-meet route", () => {
+
+    // Arrange
+    NewMeetPage.mockImplementation(() => <div>NewMeetingDetailsPage</div>);
+
+    // Act
+    render(
+      <MemoryRouter initialEntries={[ '/new-meet' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+
+    // Assert
+    expect(screen.getByText("NewMeetingDetailsPage")).toBeInTheDocument();
+    
+})
+             //Favorite
+test("Should render FAVORITE meetups page on favorite route", () => {
+
+    // Arrange
+    Favorites.mockImplementation(() => <div>FavoriteMeetingDetailsPage</div>);
+
+    // Act
+    render(
+      <MemoryRouter initialEntries={[ '/favorites' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+
+    // Assert
+    expect(screen.getByText("FavoriteMeetingDetailsPage")).toBeInTheDocument();
+    
+})
